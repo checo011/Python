4f67e7de562a7ff66db2a35ebe937cc9e576898b
checo011 <729048152@qq.com>
2023-04-21 11:07:38 +0800

    Python100天

--- Python-100天/Day01-15/02.语言元素.py
+++ Python-100天/Day01-15/02.语言元素.py
@@ -1,7 +1,9 @@
-# 练习1：华氏温度转换为摄氏温度。提示：华氏温度到摄氏温度的转换公式为：$C=(F - 32) \div 1.8$。\
-
-huashi = int(input("华氏温度:"))
-
-sheshi = (huashi - 32) / 1.8
-
-print('摄氏温度是：', sheshi)
+x = float(input('x = '))
+if x > 1:
+    y = 3 * x - 5
+else:
+    if x >= -1:
+        y = x + 2
+    else:
+        y = 5 * x + 3
+print('f(%.2f) = %.2f' % (x, y))
