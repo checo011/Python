2681fb09814d9dc5228c84c96cc5b9a439b6274e
checo011 <729048152@qq.com>
2023-04-24 20:37:37 +0800

    Python100天

--- 1.py
+++ 1.py
@@ -1,4 +1,2 @@
-fruits = ['grape', 'apple', 'strawberry', 'waxberry']
-fruits += ['pitaya', 'pear', 'mango']
-
-print(fruits)
+set4 = {num for num in range(1, 100) if num % 3 == 0 or num % 5 == 0}
+print(set4)
