7ac19edfc3660a51387631983413a7cf809f57b3
Checo011 <729048152@qq.com>
2023-05-07 15:27:24 +0800

    练习

--- 小甲鱼_Python/01猜数游戏.py
+++ 小甲鱼_Python/01猜数游戏.py
@@ -1,13 +1,16 @@
 # 猜数字游戏
 import random
 
+counte = 3
 ram_nums = int(random.randint(1, 10))
-nums = int(input('请你猜一猜密码是多少吧:'))
-while True:
+
+while counte > 0:
+    counte -= 1
+    nums = int(input('请你猜一猜密码是多少吧:'))
     if nums > ram_nums:
         print('大了')
-    if nums < ram_nums:
+    elif nums < ram_nums:
         print('小了')
     else:
         print('对了')
-        exit()
+        break
